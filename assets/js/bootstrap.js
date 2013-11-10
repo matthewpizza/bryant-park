27da93db92c8329a184a70f90223dae9f7fb4dcf
Matthew Spencer <hello@thisismatthew.com>
2013-11-10 15:33:58 -0500

    Initial Commit

---
+++ assets/js/bootstrap.js
@@ -0,0 +1,160 @@
+/**
+ *		js module:
+ *			bootstrap.js
+ *
+ *		desc:
+ *			Controls the basics of initializing site js code.
+ *			Various modules register their initialization needs with this
+ *			module. Once the document is ready, this module is activated,
+ *			initializing various site sub-modules.
+ *
+ *		requires:
+ *			jQuery
+ */
+var app = (function( app ) {
+	
+	if ( typeof app.initialized == 'undefined' ) app.initialized = false;
+
+	/* define new module */
+	app.bootstrap = (function(){
+		
+		// vars
+		var $ = null,
+			debug = _debug,
+			_init_callbacks = [],
+			testing = false
+		;
+			
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+		
+		function _init() {
+			
+			if ( app.initialized ) return false; // the app has already been initialized
+
+			// store ref to jQuery
+			$ = jQuery;
+
+			_onload();
+
+			testing = false;
+			// debug( 'We are testing' );
+
+			if (
+				_is_touch_device() ||
+				testing === true
+			) {
+				$.getScript( 'assets/js/jquery.tap.min.js' )
+					.done(function( script, textStatus ) {
+						$('html').addClass('touch');
+						debug( textStatus );
+					})
+				;
+			}
+			
+			for (var i=0; i < _init_callbacks.length; i++) {
+				_init_callbacks[i]();
+			}
+
+			app.initialized = true;
+		}
+		
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+		
+		function _register( callback ) {
+			
+			_init_callbacks.push( callback );
+			
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+		
+		function _debug( obj, showCaller ) {
+			if ( ( typeof console == "object" ) && ( console.log ) ) {
+				if (showCaller) {
+					console.log( arguments.callee.caller.name + ':' );
+				}
+				console.log( obj );
+			}
+			return false;
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		function _is_touch_device() {
+			return 'ontouchstart' in window || // works on most browsers 
+					'onmsgesturechange' in window; // works on ie10
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		function _onload() {
+			$('.no-js-message').remove();
+
+			$('<div />', {
+				class: 'date'
+			}).appendTo( 'header' );
+			$('<div />', {
+				class: 'image'
+			}).appendTo( 'main' );
+
+			// load visual controls
+			_load_controls();
+
+			// load overlay html
+			_preload_overlay();
+
+			// load keyboard shortcuts
+			if ( ! _is_touch_device() ) {
+				_preload_shortcuts();
+			}
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		function _preload_overlay() {
+			$('<div />', {
+				class: 'overlay hide'
+			}).appendTo( 'body' );
+
+			$('<div />', {
+				class: 'icon-close'
+			}).appendTo( '.overlay' );
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		function _preload_shortcuts() {
+			$.ajax({
+				url: 'assets/includes/shortcuts.html',
+				cache: false
+			}).done(function( html ) {
+				$( '.overlay' ).append( html );
+			});
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		function _load_controls() {
+			$.ajax({
+				url: 'assets/includes/controls.html',
+				cache: false
+			}).done(function( html ) {
+				$( html ).appendTo( 'main' ).show( 'slow' );
+			});
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+		
+		/* return public-facing methods and/or vars */
+		return {
+			init : _init,
+			register : _register,
+			debug : _debug,
+			is_touch_device : _is_touch_device,
+		};
+		
+	}());
+	
+	return app; /* return augmented app object */
+	
+}( app || {} )); /* import app if exists, or create new */
---
+++ assets/js/slideshow.js
@@ -0,0 +1,435 @@
+/**
+ *		js module:
+ *			slideshow.js
+ *
+ *		desc:
+ *			Loops through json
+ *			Displays images
+ *
+ *		requires:
+ *			jQuery
+ */
+var app = (function(app, $) {
+	
+	app.slideshow = (function($){
+
+		var count = 0, // Global counter, everyone gets on this
+			total = null,
+			date = null,
+			link = null,
+			img = null,
+			timer = null,
+			is_playing = false,
+			modal_visible = false,
+			keys_info_visible = false,
+			project_info_visible = false,
+			debug = app.bootstrap.debug
+		;
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+		
+		function _init() {
+
+
+			$.getJSON('assets/json/images.json').done(function(data) {
+
+				total = data.length - 1;
+
+				_load( count, data );
+
+				_slideshow( data );
+
+			});
+
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		function _slideshow( data ) {
+
+			if ( $('html').hasClass('touch') ) {
+
+				_init_touch( data );
+
+			} else {
+
+				_init_keyboard( data );
+				_init_controls( data );
+				_init_close_modal();
+
+			}
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		function _get_date( date ) {
+			var d = date,
+				month = d.getMonth(),
+				day = d.getDate(),
+				year = d.getFullYear(),
+				month_name = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ],
+				formatted_date = month_name[month] + ' ' + day + ', ' + year;
+
+			return formatted_date;
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		function _load( count, data ) {
+			if ( count !== 0 )
+				$('.restart.disabled').removeClass('disabled');
+
+			date = _get_date( new Date(data[count].date * 1000) );
+			link = data[count].link;
+			$('.date').html('<a href="' + link + '" target="_blank">' + date + '</a>');
+
+			img = data[count].file;
+			$('.image').css({ 'background-image':  'url(' + img + ')' });
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		function _init_keyboard( data ) {
+			
+			var keys = {};
+
+			$(document).keydown(function(e){
+				keys['key_' + e.which] = true;
+
+				// left arrow
+				if ( keys.key_37 ) {
+					if ( modal_visible === false ) {
+						_prev(data);
+						$('.previous').addClass('active');
+					}
+				}
+
+				// right arrow
+				if ( keys.key_39 ) {
+					if ( modal_visible === false ) {
+						_next(data);
+						$('.next').addClass('active');
+					}
+				}
+
+				// gr
+				if ( keys.key_71 && keys.key_82 ) {
+					if ( modal_visible === false ) {
+						_random(0, total, data);
+						$('.random').addClass('active');
+					}
+				}
+
+				// gi
+				if ( keys.key_71 && keys.key_73 ) {
+					if ( modal_visible === false ) {
+						if ( count !== 0 ) {
+							_restart( data );
+							$('.restart').addClass('active');
+						}
+					}
+				}
+
+				// space
+				if ( keys.key_32 ) {
+					if ( modal_visible === false ) {
+						if ( is_playing === false ) {
+							_play( data );
+						} else {
+							_pause();
+						}
+					}
+				}
+
+				// shift ?
+				if ( keys.key_16 && keys.key_191 ) {
+					_keyboard_shortcuts_info();
+				}
+				// esc 
+				if ( keys.key_27 ) {
+					if ( keys_info_visible === true ) {
+						_keyboard_shortcuts_info();
+					}
+				}
+
+				if (
+					keys.key_37 ||
+					keys.key_39 ||
+					( keys.key_71 && keys.key_82 ) ||
+					( keys.key_71 && keys.key_73 )
+				) {
+					_pause();
+					e.preventDefault();
+				}
+			}).keyup(function (e) {
+				delete keys['key_' + e.which];
+
+				$('a.active').removeClass('active');
+			});
+
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		function _init_controls( data ) {
+
+			$('.controls a').on('click', function(e) {
+
+				var class_name = $(this).attr( 'class' );
+
+				switch( class_name ) {
+					case 'random':
+						_random( 0, total, data );
+
+						if ( is_playing === true ) {
+							_pause();
+						}
+					break;
+
+					case 'restart':
+						if ( count !== 0 ) {
+							_restart( data );
+						}
+
+						if ( is_playing === true ) {
+							_pause();
+						}
+					break;
+
+					case 'next':
+						_next( data );
+
+						if ( is_playing === true ) {
+							_pause();
+						}
+					break;
+
+					case 'previous':
+						_prev( data );
+
+						if ( is_playing === true ) {
+							_pause();
+						}
+					break;
+
+					case 'play':
+						$(this).hide();
+						$('.pause').show();
+
+						_play( data );
+					break;
+
+					case 'pause':
+						_pause();
+					break;
+				}
+
+				e.preventDefault();
+			});
+			
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		function _init_touch( data ) {
+
+			debug('Touch!');
+
+			$('.controls a').on('tap', function(e) {
+
+				var class_name = $(this).attr( 'class' );
+
+				switch( class_name ) {
+					case 'random':
+						_random( 0, total, data );
+
+						if ( is_playing === true ) {
+							_pause();
+						}
+					break;
+
+					case 'restart':
+						if ( count !== 0 ) {
+							_restart( data );
+						}
+
+						if ( is_playing === true ) {
+							_pause();
+						}
+					break;
+
+					case 'next':
+						_next( data );
+
+						if ( is_playing === true ) {
+							_pause();
+						}
+					break;
+
+					case 'previous':
+						_prev( data );
+
+						if ( is_playing === true ) {
+							_pause();
+						}
+					break;
+
+					case 'play':
+						$(this).hide();
+						$('.pause').show();
+
+						_play( data );
+					break;
+
+					case 'pause':
+						_pause();
+					break;
+				}
+
+				e.preventDefault();
+			});
+
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		function _init_close_modal() {
+
+			// close modals
+			$('.icon-close').click(function(){
+				
+				if ( keys_info_visible === true ) {
+					_keyboard_shortcuts_info();
+				}
+				
+				if ( project_info_visible === true ) {
+					_project_info();
+				}
+				
+			});
+
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		function _restart( data ) {
+			count = 0;
+			_load( count, data );
+
+			window.setTimeout(function(){
+				$('.restart').addClass('disabled');
+			}, 200);
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		function _random( min, max, data ) {
+			count = Math.floor( Math.random() * ( min - max + 1 ) + max);
+
+			_load( count, data );
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		function _next( data ) {
+			count++;
+
+			// loop around to the beginning							
+			if ( count > total )
+				count = 0;
+
+			_load( count, data );
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		function _prev( data ) {
+			count--;
+
+			// loop around to the end
+			if ( count === -1 )
+				count = total;
+
+			_load( count, data );
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		function _play( data ) {
+			is_playing = true;
+
+			_next( data );
+
+			clearInterval( timer );
+			timer = setInterval( function() {
+				_next( data );
+			}, 500 );
+
+			$('.pause').show();
+			$('.play').hide();
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		function _pause() {
+			is_playing = false;
+
+			clearInterval( timer );
+			$('.play').show();
+			$('.pause').hide();
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		function _toggle_modal() {
+			if ( modal_visible === false ) {
+				// show modal
+				$('.overlay').show();
+				modal_visible = true;
+			} else {
+				// hide modal
+				$('.overlay').hide();
+				modal_visible = false;
+			}
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		function _keyboard_shortcuts_info() {
+			if ( keys_info_visible === false ) {
+				$('.shortcuts').show();
+				_toggle_modal();
+				keys_info_visible = true;
+			} else {
+				_toggle_modal();
+				$('.shortcuts').hide();
+				keys_info_visible = false;
+			}
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		function _project_info() {
+			if ( project_info_visible === false ) {
+				$('.overlay, .info').show();
+				project_info_visible = true;
+			} else {
+				$('.overlay, .info').hide();
+				project_info_visible = false;
+			}
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+		
+		/* return public-facing methods and/or vars */
+		return {
+			init : _init
+		};
+		
+	}($));
+	
+	return app; /* return augmented app object */
+	
+}( app || {}, jQuery )); /* import app if exists, or create new */
+
+app.bootstrap.register( app.slideshow.init );
